c47d9d67daf5f2c47b22f8128f55c2ec69a35211
LewisHe <streeb30@gmail.com>
2025-04-13 11:51:42 -0400

    Update

--- Quantitative Project/Trend-following.py
+++ Quantitative Project/Trend-following.py
@@ -10,13 +10,42 @@ data['Signal'] = 0
 data.loc[data['SMA_short'] > data['SMA_long'], 'Signal'] = 1
 data['Position'] = data['Signal'].diff()
 
-# print(data[data['Position'] == 1])
-# print(data[data['Position'] == -1])
+init_cash = 10000
+cash = init_cash
+shares = 0
+portfolio_value = []
+in_position = False
 
-# print(data.head(10))
-# print(data[['Close', 'SMA_short', 'SMA_long']].describe())
+for i in range(len(data)):
+    price = data['Close'].iloc[i]
+    signal = data['Position'].iloc[i]
 
+    if signal == 1 and not in_position:
+        shares = cash/price
+        cash = 0
+        in_position = True
+
+    elif signal == -1 and in_position:
+        cash = shares * price
+        shares = 0
+        in_position = False
+
+    total_value = cash + shares * price
+    portfolio_value.append(total_value)
+
+data['Portfolio'] = portfolio_value
+
+# Drawing the total value we get from day to day
 plt.figure(figsize=(14,7))
+plt.plot(data['Portfolio'], label='Strategy Portfolio Value ($)')
+plt.title('Actual Account Value Over Time (Double Moving Average Strategy)')
+plt.xlabel('Date')
+plt.ylabel('Portfolio Value')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# Drawing the Close, short and long average line, and buying, selling point
 plt.plot(data['Close'], label='Close Price', alpha=0.5)
 plt.plot(data['SMA_short'], label='20-Day SMA')
 plt.plot(data['SMA_long'], label='50-Day SMA')
@@ -25,7 +54,12 @@ plt.plot(data[data['Position'] == 1].index, data['SMA_short'][data['Position'] =
          '^', markersize=10, color='g', label='Buy Signal')
 plt.plot(data[data['Position'] == -1].index, data['SMA_long'][data['Position'] == -1],
          'v', markersize=10, color='r', label='Sell Signal')
-
 plt.legend()
 plt.title('AAPL Dual moving Average S')
 plt.show()
+
+# For print the actual buying and selling signal
+# print(data[data['Position'] == 1])
+# print(data[data['Position'] == -1])
+# print(data.head(10))
+# print(data[['Close', 'SMA_short', 'SMA_long']].describe())
